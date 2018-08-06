c6c321dcfeb140dbfad2987414e62ef80ffa87fe
ronijr <ronijakarianto01@gmail.com>
2018-08-06 07:12:16 +0700

    Clear Fix

--- app/src/main/java/com/example/apps/com/sistempakar/HasilDiagnosaActivity.java
+++ app/src/main/java/com/example/apps/com/sistempakar/HasilDiagnosaActivity.java
@@ -1,8 +1,11 @@
 package com.example.apps.com.sistempakar;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import butterknife.BindView;
@@ -10,15 +13,100 @@ import butterknife.ButterKnife;
 
 public class HasilDiagnosaActivity extends AppCompatActivity {
 
-    @BindView(R.id.persen) TextView persen;
-    @BindView(R.id.penyakit) TextView penyakit;
+    @BindView(R.id.persen1) TextView persen1;
+    @BindView(R.id.penyakit1) TextView penyakit1;
+    @BindView(R.id.deskripsi1) TextView desk1;
+    @BindView(R.id.solusi1)TextView solusi1;
+
+    @BindView(R.id.persen2) TextView persen2;
+    @BindView(R.id.penyakit2) TextView penyakit2;
+    @BindView(R.id.deskripsi2) TextView desk2;
+    @BindView(R.id.solusi2)TextView solusi2;
+
+    @BindView(R.id.persen3) TextView persen3;
+    @BindView(R.id.penyakit3) TextView penyakit3;
+    @BindView(R.id.deskripsi3) TextView desk3;
+    @BindView(R.id.solusi3)TextView solusi3;
+
+    @BindView(R.id.persen4) TextView persen4;
+    @BindView(R.id.penyakit4) TextView penyakit4;
+    @BindView(R.id.deskripsi4) TextView desk4;
+    @BindView(R.id.solusi4)TextView solusi4;
+
+    @BindView(R.id.persen5) TextView persen5;
+    @BindView(R.id.penyakit5) TextView penyakit5;
+    @BindView(R.id.deskripsi5) TextView desk5;
+    @BindView(R.id.solusi5)TextView solusi5;
+
+    @BindView(R.id.layout1) LinearLayout layout1;
+    @BindView(R.id.layout2) LinearLayout layout2;
+    @BindView(R.id.layout3) LinearLayout layout3;
+    @BindView(R.id.layout4) LinearLayout layout4;
+    @BindView(R.id.layout5) LinearLayout layout5;
+
+    Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hasil_diagnosa);
         ButterKnife.bind(this);
-        Intent intent = getIntent();
-        penyakit.setText(intent.getStringExtra("penyakit"));
-        persen.setText(intent.getStringExtra("persen")+"%");
+        intent = getIntent();
+        inisialisasiView();
+
+    }
+
+    @SuppressLint("SetTextI18n")
+    private void inisialisasiView(){
+        if(intent.getStringExtra("code1") != null){
+            layout1.setVisibility(View.VISIBLE);
+            persen1.setText(intent.getStringExtra("persen1")+"%");
+            penyakit1.setText(intent.getStringExtra("penyakit1"));
+            desk1.setText(getString(R.string.desk1));
+            solusi1.setText(getString(R.string.solusi1));
+        }else{
+            layout1.setVisibility(View.GONE);
+        }
+
+        if(intent.getStringExtra("code2") != null){
+            layout2.setVisibility(View.VISIBLE);
+            persen2.setText(intent.getStringExtra("persen2")+"%");
+            penyakit2.setText(intent.getStringExtra("penyakit2"));
+            desk2.setText(getString(R.string.desk2));
+            solusi2.setText(getString(R.string.solusi2));
+        }else{
+            layout2.setVisibility(View.GONE);
+        }
+
+
+        if(intent.getStringExtra("code3") != null){
+            layout3.setVisibility(View.VISIBLE);
+            persen3.setText(intent.getStringExtra("persen3")+"%");
+            penyakit3.setText(intent.getStringExtra("penyakit3"));
+            desk3.setText(getString(R.string.desk3));
+            solusi3.setText(getString(R.string.solusi3));
+        }else{
+            layout3.setVisibility(View.GONE);
+        }
+
+        if(intent.getStringExtra("code4") != null){
+            layout4.setVisibility(View.VISIBLE);
+            persen4.setText(intent.getStringExtra("persen4")+"%");
+            penyakit4.setText(intent.getStringExtra("penyakit4"));
+            desk4.setText(getString(R.string.desk4));
+            solusi4.setText(getString(R.string.solusi4));
+        }else{
+            layout4.setVisibility(View.GONE);
+        }
+
+
+        if(intent.getStringExtra("code5") != null){
+            layout5.setVisibility(View.VISIBLE);
+            persen5.setText(intent.getStringExtra("persen5")+"%");
+            penyakit5.setText(intent.getStringExtra("penyakit5"));
+            desk5.setText(getString(R.string.desk5));
+            solusi5.setText(getString(R.string.solusi5));
+        }else{
+            layout5.setVisibility(View.GONE);
+        }
     }
 }
--- app/src/main/java/com/example/apps/com/sistempakar/MainActivity.java
+++ app/src/main/java/com/example/apps/com/sistempakar/MainActivity.java
@@ -65,6 +65,9 @@ public class MainActivity extends AppCompatActivity {
 
     Database database;
     Diagnosa modeldiagnosa;
+    String penyakit1,penyakit2,penyakit3,penyakit4,penyakit5 = "";
+    String persen1,persen2,persen3,persen4,persen5 ="";
+    String code1,code2,code3,code4,code5="";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -148,108 +151,218 @@ public class MainActivity extends AppCompatActivity {
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ArrayList<String> objects = new ArrayList<>();
-                String penyakit=null;
-                String akhir=null;
-                   if(g01.isChecked() && g02.isChecked() && g03.isChecked() && g04.isChecked()){
-                       double cf1p1 = 0,cf2p1 =0;
-                       double cf3p1 = 0,cf4p1= 0;
-                       cf1p1 = Math.floor((database.getDiagnosa("G01").getMb() * database.getDiagnosa("G01").getMd()) *100)/100;
-                       cf2p1 = Math.floor((database.getDiagnosa("G02").getMb() * database.getDiagnosa("G02").getMd())*100)/100;
-                       cf3p1 = Math.floor((database.getDiagnosa("G03").getMb() * database.getDiagnosa("G03").getMd() * 100)) / 100;
-                       cf4p1 = Math.floor((database.getDiagnosa("G04").getMb() * database.getDiagnosa("G04").getMd()) * 100) /100;
-
-                       double cfpp1_2 = cf1p1 + cf2p1 * (1 - cf1p1);
-                       double cfpp1_3 = cfpp1_2 + cf3p1 * (1 - cfpp1_2);
-                       double cfpp2_4 = cfpp1_3 + cf4p1 * (1 - cfpp1_3);
-                       cfpp2_4 = Math.floor(cfpp2_4 * 100) / 100;
-                       double persen = cfpp2_4 * 100;
-                       akhir = String.valueOf(Math.round(persen));
-                       penyakit = "Skizofrenia Paranoid";
-                   }else if(g05.isChecked() && g06.isChecked() && g07.isChecked() && g08.isChecked() && g09.isChecked()){
-                       double cf1p2 = 0,cf2p2 =0;
-                       double cf3p2 = 0,cf4p2= 0;
-                       double cf5p2;
-                       cf1p2 = Math.floor((database.getDiagnosa("G05").getMb() * database.getDiagnosa("G05").getMd()) *100)/100;
-                       cf2p2 = Math.floor((database.getDiagnosa("G06").getMb() * database.getDiagnosa("G06").getMd())*100)/100;
-                       cf3p2 = Math.floor((database.getDiagnosa("G07").getMb() * database.getDiagnosa("G07").getMd() * 100)) / 100;
-                       cf4p2 = Math.floor((database.getDiagnosa("G08").getMb() * database.getDiagnosa("G08").getMd()) * 100) /100;
-                       cf5p2 = Math.floor((database.getDiagnosa("G09").getMb() * database.getDiagnosa("G09").getMd()) * 100) /100;
-
-                       double cfpp2_1 = cf1p2 + cf2p2 * (1 - cf1p2);
-                       double cfpp2_2 = cfpp2_1 + cf3p2 * (1 - cfpp2_1);
-                       double cfpp2_3 = cfpp2_2 + cf4p2 * (1 - cfpp2_2);
-                       double cfpp2_4 = cfpp2_3 + cf5p2 * (1 - cfpp2_3);
-                       cfpp2_4 = Math.floor(cfpp2_4 * 100) / 100;
-                       double persen = cfpp2_4 * 100;
-                       akhir = String.valueOf(Math.round(persen));
-                       penyakit = "Skizofrenia Hiberfrenik";
-                   } else if(g10.isChecked() && g11.isChecked() && g12.isChecked() && g13.isChecked() && g14.isChecked()){
-                       double cf1p3 = 0,cf2p3 =0;
-                       double cf3p3 = 0,cf4p3= 0;
-                       double cf5p3;
-                       cf1p3 = Math.floor((database.getDiagnosa("G10").getMb() * database.getDiagnosa("G10").getMd()) *100)/100;
-                       cf2p3 = Math.floor((database.getDiagnosa("G11").getMb() * database.getDiagnosa("G11").getMd())*100)/100;
-                       cf3p3 = Math.floor((database.getDiagnosa("G12").getMb() * database.getDiagnosa("G12").getMd() * 100)) / 100;
-                       cf4p3 = Math.floor((database.getDiagnosa("G13").getMb() * database.getDiagnosa("G13").getMd()) * 100) /100;
-                       cf5p3 = Math.floor((database.getDiagnosa("G14").getMb() * database.getDiagnosa("G14").getMd()) * 100) /100;
-
-                       double cfpp3_1 = cf1p3 + cf2p3 * (1 - cf1p3);
-                       double cfpp3_2 = cfpp3_1 + cf3p3 * (1 - cfpp3_1);
-                       double cfpp3_3 = cfpp3_2 + cf4p3 * (1 - cfpp3_2);
-                       double cfpp3_4 = cfpp3_3 + cf5p3 * (1 - cfpp3_3);
-                       cfpp3_4 = Math.floor(cfpp3_4 * 100) / 100;
-                       double persen = cfpp3_4 * 100;
-                       akhir = String.valueOf(Math.round(persen));
-                       penyakit = "Skizofrenia Katatonik";
-                   }else if(g10.isChecked() && g11.isChecked() && g12.isChecked() && g13.isChecked() && g14.isChecked()){
-                       double cf1p4 = 0,cf2p4 =0;
-                       double cf3p4 = 0,cf4p4= 0;
-                       double cf5p4;
-                       cf1p4 = Math.floor((database.getDiagnosa("G15").getMb() * database.getDiagnosa("G15").getMd()) *100)/100;
-                       cf2p4 = Math.floor((database.getDiagnosa("G16").getMb() * database.getDiagnosa("G16").getMd())*100)/100;
-                       cf3p4 = Math.floor((database.getDiagnosa("G17").getMb() * database.getDiagnosa("G17").getMd() * 100)) / 100;
-                       cf4p4 = Math.floor((database.getDiagnosa("G18").getMb() * database.getDiagnosa("G18").getMd()) * 100) /100;
-
-                       double cfpp4_1 = cf1p4 + cf2p4 * (1 - cf1p4);
-                       double cfpp4_2 = cfpp4_1 + cf3p4 * (1 - cfpp4_1);
-                       double cfpp4_3 = cfpp4_2 + cf4p4 * (1 - cfpp4_2);
-                       cfpp4_3 = Math.floor(cfpp4_3 * 100) / 100;
-                       double persen = cfpp4_3 * 100;
-                       akhir = String.valueOf(Math.round(persen));
-                       penyakit = "Skizofrenia Residual";
-                   }else if(g19.isChecked() && g20.isChecked() && g21.isChecked() && g22.isChecked() && g23.isChecked()){
-                       double cf1p5 = 0,cf2p5 =0;
-                       double cf3p5 = 0,cf4p5= 0;
-                       double cf5p5;
-                       cf1p5 = Math.floor((database.getDiagnosa("G19").getMb() * database.getDiagnosa("G19").getMd()) *100)/100;
-                       cf2p5 = Math.floor((database.getDiagnosa("G20").getMb() * database.getDiagnosa("G20").getMd())*100)/100;
-                       cf3p5 = Math.floor((database.getDiagnosa("G21").getMb() * database.getDiagnosa("G21").getMd() * 100)) / 100;
-                       cf4p5 = Math.floor((database.getDiagnosa("G22").getMb() * database.getDiagnosa("G22").getMd()) * 100) /100;
-                       cf5p5 = Math.floor((database.getDiagnosa("G22").getMb() * database.getDiagnosa("G22").getMd()) * 100) /100;
-
-                       double cfpp5_1 = cf1p5 + cf2p5 * (1 - cf1p5);
-                       double cfpp5_2 = cfpp5_1 + cf3p5 * (1 - cfpp5_1);
-                       double cfpp5_3 = cfpp5_2 + cf4p5 * (1 - cfpp5_2);
-                       double cfpp5_4 = cfpp5_3 + cf4p5 * (1 - cfpp5_3);
-                       cfpp5_4 = Math.floor(cfpp5_4 * 100) / 100;
-                       double persen = cfpp5_4 * 100;
-                       akhir = String.valueOf(Math.round(persen));
-                           penyakit = "Skizofrenia Simplek";
-                   } else {
-                       akhir = "0";
-                       penyakit = "Tidak diketahui";
-                   }
-
-
-
-
-                Intent intent = new Intent(MainActivity.this, HasilDiagnosaActivity.class);
-                intent.putExtra("persen",akhir);
-                intent.putExtra("penyakit",penyakit);
-                startActivity(intent);
+                if(!g01.isChecked() && !g02.isChecked()  && !g03.isChecked() && !g04.isChecked() && !g05.isChecked() && !g06.isChecked() && !g07.isChecked() && !g08.isChecked() && !g09.isChecked() && !g10.isChecked() && !g11.isChecked()
+                        && !g12.isChecked() && !g13.isChecked() && !g14.isChecked() && !g15.isChecked() && !g16.isChecked()
+                        && !g17.isChecked() && !g18.isChecked() && !g19.isChecked() &&
+                        !g20.isChecked() && !g21.isChecked() && !g22.isChecked() && !g23.isChecked()){
+                    Toast.makeText(getApplicationContext(),"Pilih gejala",Toast.LENGTH_SHORT).show();
+                }else{
+                    caculationDiagnosa();
+                }
             }
         });
+
+    }
+
+    private void caculationDiagnosa(){
+        Intent intent = new Intent(MainActivity.this, HasilDiagnosaActivity.class);
+
+        if(g01.isChecked() || g02.isChecked() || g03.isChecked() || g04.isChecked()){
+            double cf1p1 = 0,cf2p1 =0;
+            double cf3p1 = 0,cf4p1= 0;
+            if(g01.isChecked()){
+                cf1p1 = Math.floor((database.getDiagnosa("G01").getMb() * database.getDiagnosa("G01").getMd()) *100)/100;
+            }
+
+            if(g02.isChecked()){
+                cf2p1 = Math.floor((database.getDiagnosa("G02").getMb() * database.getDiagnosa("G02").getMd())*100)/100;
+            }
+
+            if(g03.isChecked()){
+                cf3p1 = Math.floor((database.getDiagnosa("G03").getMb() * database.getDiagnosa("G03").getMd() * 100)) / 100;
+            }
+
+            if(g04.isChecked()){
+                cf4p1 = Math.floor((database.getDiagnosa("G04").getMb() * database.getDiagnosa("G04").getMd()) * 100) /100;
+            }
+
+            double cfpp1_2 = cf1p1 + cf2p1 * (1 - cf1p1);
+            double cfpp1_3 = cfpp1_2 + cf3p1 * (1 - cfpp1_2);
+            double cfpp2_4 = cfpp1_3 + cf4p1 * (1 - cfpp1_3);
+            cfpp2_4 = Math.floor(cfpp2_4 * 100) / 100;
+            double persen = cfpp2_4 * 100;
+            persen1 = String.valueOf(Math.round(persen));
+            penyakit1 = "Skizofrenia Paranoid";
+            code1 ="P1";
+            intent.putExtra("persen1",persen1);
+            intent.putExtra("penyakit1",penyakit1);
+            intent.putExtra("code1",code1);
+
+        }
+
+
+        if(g05.isChecked() || g06.isChecked() || g07.isChecked() || g08.isChecked() || g09.isChecked()){
+            double cf1p2 = 0,cf2p2 =0;
+            double cf3p2 = 0,cf4p2= 0;
+            double cf5p2 = 0;
+            if(g05.isChecked()){
+                cf1p2 = Math.floor((database.getDiagnosa("G05").getMb() * database.getDiagnosa("G05").getMd()) *100)/100;
+            }
+
+            if(g06.isChecked()){
+                cf2p2 = Math.floor((database.getDiagnosa("G06").getMb() * database.getDiagnosa("G06").getMd())*100)/100;
+            }
+
+            if(g07.isChecked()){
+                cf3p2 = Math.floor((database.getDiagnosa("G07").getMb() * database.getDiagnosa("G07").getMd() * 100)) / 100;
+            }
+
+
+            if(g08.isChecked() ){
+                cf4p2 = Math.floor((database.getDiagnosa("G08").getMb() * database.getDiagnosa("G08").getMd()) * 100) /100;
+            }
+
+
+            if(g09.isChecked()){
+                cf5p2 = Math.floor((database.getDiagnosa("G09").getMb() * database.getDiagnosa("G09").getMd()) * 100) /100;
+            }
+
+            double cfpp2_1 = cf1p2 + cf2p2 * (1 - cf1p2);
+            double cfpp2_2 = cfpp2_1 + cf3p2 * (1 - cfpp2_1);
+            double cfpp2_3 = cfpp2_2 + cf4p2 * (1 - cfpp2_2);
+            double cfpp2_4 = cfpp2_3 + cf5p2 * (1 - cfpp2_3);
+            cfpp2_4 = Math.floor(cfpp2_4 * 100) / 100;
+            double persen = cfpp2_4 * 100;
+            persen2 = String.valueOf(Math.round(persen));
+            penyakit2 = "Skizofrenia Hiberfrenik";
+            code2 = "P2";
+
+            //Penyakit 2
+            intent.putExtra("persen2",persen2);
+            intent.putExtra("penyakit2",penyakit2);
+            intent.putExtra("code2",code2);
+
+        }
+
+        if(g10.isChecked() || g11.isChecked() || g12.isChecked() || g13.isChecked() || g14.isChecked()){
+            double cf1p3 = 0,cf2p3 =0;
+            double cf3p3 = 0,cf4p3= 0;
+            double cf5p3 = 0;
+
+            if(g10.isChecked()){
+                cf1p3 = Math.floor((database.getDiagnosa("G10").getMb() * database.getDiagnosa("G10").getMd()) *100)/100;
+            }
+
+            if(g11.isChecked()){
+                cf2p3 = Math.floor((database.getDiagnosa("G11").getMb() * database.getDiagnosa("G11").getMd())*100)/100;
+            }
+
+            if(g12.isChecked()){
+                cf3p3 = Math.floor((database.getDiagnosa("G12").getMb() * database.getDiagnosa("G12").getMd() * 100)) / 100;
+            }
+
+            if(g13.isChecked()){
+                cf4p3 = Math.floor((database.getDiagnosa("G13").getMb() * database.getDiagnosa("G13").getMd()) * 100) /100;
+            }
+
+            if(g14.isChecked()){
+                cf5p3 = Math.floor((database.getDiagnosa("G14").getMb() * database.getDiagnosa("G14").getMd()) * 100) /100;
+            }
+
+            double cfpp3_1 = cf1p3 + cf2p3 * (1 - cf1p3);
+            double cfpp3_2 = cfpp3_1 + cf3p3 * (1 - cfpp3_1);
+            double cfpp3_3 = cfpp3_2 + cf4p3 * (1 - cfpp3_2);
+            double cfpp3_4 = cfpp3_3 + cf5p3 * (1 - cfpp3_3);
+            cfpp3_4 = Math.floor(cfpp3_4 * 100) / 100;
+            double persen = cfpp3_4 * 100;
+            persen3 = String.valueOf(Math.round(persen));
+            penyakit3 = "Skizofrenia Katatonik";
+            code3 = "P3";
+            //Penyakit 3
+            intent.putExtra("persen3",persen3);
+            intent.putExtra("penyakit3",penyakit3);
+            intent.putExtra("code3",code3);
+
+        }
+
+        if(g15.isChecked() || g16.isChecked() || g17.isChecked() || g18.isChecked()){
+            double cf1p4 = 0,cf2p4 =0;
+            double cf3p4 = 0,cf4p4= 0;
+
+            if(g15.isChecked()){
+                cf1p4 = Math.floor((database.getDiagnosa("G15").getMb() * database.getDiagnosa("G15").getMd()) *100)/100;
+            }
+
+            if(g16.isChecked()){
+                cf2p4 = Math.floor((database.getDiagnosa("G16").getMb() * database.getDiagnosa("G16").getMd())*100)/100;
+            }
+
+            if(g17.isChecked()){
+                cf3p4 = Math.floor((database.getDiagnosa("G17").getMb() * database.getDiagnosa("G17").getMd() * 100)) / 100;
+            }
+
+            if(g18.isChecked()){
+                cf4p4 = Math.floor((database.getDiagnosa("G18").getMb() * database.getDiagnosa("G18").getMd()) * 100) /100;
+            }
+
+            double cfpp4_1 = cf1p4 + cf2p4 * (1 - cf1p4);
+            double cfpp4_2 = cfpp4_1 + cf3p4 * (1 - cfpp4_1);
+            double cfpp4_3 = cfpp4_2 + cf4p4 * (1 - cfpp4_2);
+            cfpp4_3 = Math.floor(cfpp4_3 * 100) / 100;
+            double persen = cfpp4_3 * 100;
+            persen4 = String.valueOf(Math.round(persen));
+            penyakit4 = "Skizofrenia Residual";
+            code4 ="P4";
+
+            //Penyakit 4
+            intent.putExtra("persen4",persen4);
+            intent.putExtra("penyakit4",penyakit4);
+            intent.putExtra("code4",code4);
+        }
+
+
+        if(g19.isChecked() || g20.isChecked() || g21.isChecked() || g22.isChecked() || g23.isChecked()){
+            double cf1p5 = 0,cf2p5 =0;
+            double cf3p5 = 0,cf4p5= 0;
+            double cf5p5 = 0;
+
+            if(g19.isChecked()){
+                cf1p5 = Math.floor((database.getDiagnosa("G19").getMb() * database.getDiagnosa("G19").getMd()) *100)/100;
+            }
+
+            if(g20.isChecked()){
+                cf2p5 = Math.floor((database.getDiagnosa("G20").getMb() * database.getDiagnosa("G20").getMd())*100)/100;
+            }
+
+            if(g21.isChecked()){
+                cf3p5 = Math.floor((database.getDiagnosa("G21").getMb() * database.getDiagnosa("G21").getMd() * 100)) / 100;
+            }
+
+            if(g22.isChecked()){
+                cf4p5 = Math.floor((database.getDiagnosa("G22").getMb() * database.getDiagnosa("G22").getMd()) * 100) /100;
+            }
+
+            if(g23.isChecked()){
+                cf5p5 = Math.floor((database.getDiagnosa("G23").getMb() * database.getDiagnosa("G23").getMd()) * 100) /100;
+            }
+
+            double cfpp5_1 = cf1p5 + cf2p5 * (1 - cf1p5);
+            double cfpp5_2 = cfpp5_1 + cf3p5 * (1 - cfpp5_1);
+            double cfpp5_3 = cfpp5_2 + cf4p5 * (1 - cfpp5_2);
+            double cfpp5_4 = cfpp5_3 + cf5p5 * (1 - cfpp5_3);
+            cfpp5_4 = Math.floor(cfpp5_4 * 100) / 100;
+            double persen = cfpp5_4 * 100;
+            persen5 = String.valueOf(Math.round(persen));
+            penyakit5 = "Skizofrenia Simplek";
+            code5 ="P5";
+
+            //Penyakit 5
+            intent.putExtra("persen5",persen5);
+            intent.putExtra("penyakit5",penyakit5);
+            intent.putExtra("code5",code5);
+        }
+
+        startActivity(intent);
 
     }
 
