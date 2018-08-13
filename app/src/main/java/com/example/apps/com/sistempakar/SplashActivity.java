3532dbddda6296200d0145942ce5b6018eb5e021
ronijr <ronijakarianto01@gmail.com>
2018-08-13 18:45:56 +0700

    merger

---
+++ app/src/main/java/com/example/apps/com/sistempakar/SplashActivity.java
@@ -0,0 +1,24 @@
+package com.example.apps.com.sistempakar;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    public static int SPLAH_TIME_OUT = 3000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this,MainActivity.class));
+                finish();
+            }
+        },SPLAH_TIME_OUT);
+
+    }
+}
